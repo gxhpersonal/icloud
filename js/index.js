31084605dc0d9caa55de3f8934f68bdabda726f4
gxhpersonal <991158744@qq.com>
2016-06-18 12:01:03 +0800

    aaa

--- js/index.js
+++ js/index.js
@@ -1,47 +1,48 @@
-var icloud = angular.module('icloud',[]);
-icloud.controller('main',['$scope',function($scope){
-  $scope.savedata = function(){
-    localStorage.list = angular.toJson($scope.qingdanlist)
-  };
-  if(localStorage.list){
-    $scope.qingdanlist = angular.fromJson(localStorage.list)
-  }else{
-    $scope.qingdanlist = [];
+var todo=angular.module('todoList',[]);
+todo.controller('todoCtr',['$scope',function ($scope) {
+  $scope.todoLists=localStorage.todoList?
+  angular.fromJson(localStorage.todoList)
+  :[];
+  $scope.current=$scope.todoLists.length?$scope.todoLists[0]:null;
+  $scope.saveDate=function () {
+    localStorage.todoList=angular.toJson($scope.todoLists);
   }
-
-  $scope.currentqd = null;
-  var colors = ['purple','green','blue','yellow','brown','pink','orange'];
-  var qingdan = {};
-  $scope.addliebiao = function(){
-    var len = $scope.qingdanlist.length;
-    qingdan = {
-      id:(len === 0)?10001:(Math.max.apply('',$scope.qingdanlist.map(function(v,i){
-        return v.id
-      }))+1),
-      name:'新清单'+(len+1),
-      color:colors[len%7],
-      shixiang:[
-      ],
-    };
-    $scope.currentqd = qingdan;
-    $scope.qingdanlist.push(qingdan);
-    this.savedata();
+  $scope.addList=function () {
+    var le=$scope.todoLists.length;
+    var id=(le===0)?1001:Math.max.apply(null,$scope.todoLists.map(function (v,i) {
+      return v.id;
+    }))+1;
+    var newTodo={
+      id:id,
+      name:'新事项'+(le+1),
+      color:'color'+((le)%7),
+      item:[]
+    }
+    $scope.todoLists.push(newTodo);
+    $scope.saveDate();
+    $scope.current=newTodo;
   }
-  $scope.shanchu = function(id){
-    $scope.qingdanlist =$scope.qingdanlist.filter(function(v,i){
-      return v.id !== id;
+  $scope.cgeState=function (v) {
+    v.state=!v.state;
+    $scope.saveDate();
+  }
+  $scope.currentList=function (i) {
+     $scope.current=$scope.todoLists[i];
+  }
+  $scope.addItem=function (items) {
+    var newItem={name:'事项'+(items.length+1),state:false};
+    items.push(newItem);
+    $scope.saveDate();
+  }
+  $scope.clearDone=function (cur) {
+    cur.item=cur.item.filter(function(v,i){
+      return !v.state;
     })
-      $scope.currentqd=$scope.qingdanlist[0];
-    this.savedata()
+    $scope.saveDate();
   }
-  $scope.addshixiang = function(){
-    var shix = [{name:"事项",state:false}];
-    qingdan.shixiang = shix;
-    $scope.currentqd = qingdan;
-    $scope.qingdanlist.push(qingdan);
-    this.savedata()
-  }
-  $scope.gaibian = function(index){
-    $scope.currentqd = $scope.qingdanlist[index]
+  $scope.color=['color0','color4','color3','color1','color6','color5','color2'];
+  $scope.changeColor=function (cur,i) {
+    cur.color=$scope.color[i];
+    $scope.saveDate();
   }
 }])
